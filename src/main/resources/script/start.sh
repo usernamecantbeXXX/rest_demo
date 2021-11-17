c06df6656de77941d27789ab2d9e5ea611cd1275
许先学 <xuxianxue@bluemoon.com.cn>
2021-11-17 19:41:07 +0800

    init

---
+++ src/main/resources/script/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+PIDFILE=service.pid
+if [ ! -d "logs" ]; then
+   mkdir logs
+fi
+if [ -f "$PIDFILE" ]; then
+    echo "Service is already start ..."
+else
+    echo "Service  start ..."
+    nohup java -jar rest_demo-0.0.1-SNAPSHOT.jar 1> logs/rest_demo.out 2>&1  &
+    printf '%d' $! > $PIDFILE
+    echo "Service  start SUCCESS "
+fi
